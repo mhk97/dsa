eb2d784d82259744643c68c65789888ec06c9e76
mhk97 <149612134+mhk97@users.noreply.github.com>
2024-10-29 20:51:21 +0530

    Time: 5 ms (74.09%), Space: 44.5 MB (8.86%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,15 +1,15 @@
 class Solution {
-    public boolean isPalindrome(int temp) {
-        int res = 0;
 
-        if((temp %10 == 0 && temp != 0) || temp < 0) return false;
+    public boolean isPalindrome(int x) {
+        if (x == 0) return true;
+        if (x % 10 == 0 || x < 0) return false;
 
-        while(temp>res){
-            res = res*10 + temp%10;
-            temp = temp/10;
+        int temp = x, nn = 0;
+        while (temp > 0) {
+            nn = nn * 10 + temp % 10;
+            temp /= 10;
         }
-
-        return res == temp || temp == res/10;
-
+        // System.out.println(nn);
+        return nn == x;
     }
 }
