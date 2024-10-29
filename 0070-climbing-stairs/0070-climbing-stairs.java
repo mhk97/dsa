9ea7cd2081c9d36558001b14faa4b2e4f63965c9
mhk97 <149612134+mhk97@users.noreply.github.com>
2024-10-29 20:38:57 +0530

    Time: 0 ms (100%), Space: 40.1 MB (80.6%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -2,7 +2,14 @@ class Solution {
 
     public int climbStairs(int n) {
         int[] dp = new int[n + 1];
-        return solve(n, dp);
+
+        dp[0] = dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
+        // return solve(n, dp);
     }
 
     public int solve(int n, int[] dp) {
