6e5b7c5b07a7e4326d1e2aa9214103b5680287b8
mhk97 <149612134+mhk97@users.noreply.github.com>
2024-11-13 17:55:50 +0530

    Time: 318 ms (24.65%), Space: 58.2 MB (5%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -2,10 +2,11 @@ class Solution {
 
     public String longestPalindrome(String s) {
         int len = 0, n = s.length(), startIndex = -1;
+        Boolean cache[][] = new Boolean[1001][1001];
 
         for (int i = 0; i < n; i++) {
             for (int j = i; j < n; j++) {
-                if (isPalindrome(i, j, s) && j - i + 1 > len) {
+                if (isPalindrome(i, j, s, cache) && j - i + 1 > len) {
                     startIndex = i;
                     len = Math.max(len, j - i + 1);
                 }
@@ -15,15 +16,10 @@ class Solution {
         return s.substring(startIndex, startIndex + len);
     }
 
-    private boolean isPalindrome(int i, int j, String s) {
-        while (i < j) {
-            if (s.charAt(i) != s.charAt(j)) {
-                return false;
-            }
-            i++;
-            j--;
-        }
-
-        return true;
+    private boolean isPalindrome(int i, int j, String s, Boolean cache[][]) {
+        if (i > j) return true;
+        if (cache[i][j] != null) return cache[i][j];
+        if (s.charAt(i) != s.charAt(j)) return cache[i][j] = false;
+        return cache[i][j] = isPalindrome(i + 1, j - 1, s, cache);
     }
 }
