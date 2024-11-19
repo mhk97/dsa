bd819b5b447e95bb2d36276e9193bbcb161862ec
mhk97 <149612134+mhk97@users.noreply.github.com>
2024-11-19 18:48:45 +0530

    Time: 4 ms (55.97%), Space: 41.9 MB (21.64%) - LeetHub

--- 0778-reorganize-string/0778-reorganize-string.java
+++ 0778-reorganize-string/0778-reorganize-string.java
@@ -1,36 +1,30 @@
 class Solution {
+
     public String reorganizeString(String s) {
         int[] arr = new int[26];
-        for (char c : s.toCharArray())
-            arr[c - 'a']++;
-
-        PriorityQueue<int[]> pq = new PriorityQueue<int[]>((a, b) -> Integer.compare(b[1], a[1]));
-        for (int i = 0; i < arr.length; i++)
-            if(arr[i]>0)pq.offer(new int[]{i+'a',arr[i]});
+        for (char c : s.toCharArray()) arr[c - 'a']++;
 
         StringBuilder sb = new StringBuilder();
-        while(!pq.isEmpty()){
-            int[] first = pq.poll();
-            if(sb.length() == 0 || first[0] != sb.charAt(sb.length()-1)){
-                sb.append((char)first[0]);
-                if(--first[1] > 0){
-                    pq.offer(first);
-                }
-            }
-            else{
-                if(pq.isEmpty()) return "";
+        PriorityQueue<int[]> pq = new PriorityQueue<int[]>((a, b) -> Integer.compare(b[0], a[0]));
 
+        for (int i = 0; i < arr.length; i++) if (arr[i] > 0) pq.offer(new int[] { arr[i], i + 'a' });
+
+        while (!pq.isEmpty()) {
+            int[] first = pq.poll();
+            if (sb.length() == 0 || sb.charAt(sb.length() - 1) != ((char) first[1])) {
+                sb.append((char) first[1]); //append
+                if (--first[0] > 0) pq.offer(first);
+            } else {
+                if (pq.isEmpty()) return "";
                 int[] second = pq.poll();
-                sb.append((char)second[0]);
-                if(--second[1] > 0){
-                    pq.offer(second);
-                }
+                sb.append((char) second[1]); //append
+
+                if (--second[0] > 0) pq.offer(second);
 
                 pq.offer(first);
             }
         }
 
         return sb.toString();
-
     }
 }
