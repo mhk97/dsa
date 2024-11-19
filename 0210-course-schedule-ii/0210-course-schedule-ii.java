817c8b4b1ce436b445fa9ffdd0510afb851554ee
mhk97 <149612134+mhk97@users.noreply.github.com>
2024-11-19 19:05:02 +0530

    Time: 7 ms (39.82%), Space: 45.8 MB (61.75%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,30 +1,52 @@
 class Solution {
 
     public int[] findOrder(int V, int[][] edges) {
-        //create a map
+        //create a map n indegree
+        int[] indegree = new int[V];
         Map<Integer, List<Integer>> map = new HashMap();
         for (int[] edge : edges) {
             int u = edge[1], v = edge[0];
+            indegree[v]++;
             map.computeIfAbsent(u, k -> new ArrayList()).add(v);
         }
 
-        boolean v[] = new boolean[V];
-        boolean pv[] = new boolean[V];
-        Stack<Integer> stack = new Stack();
+        // boolean v[] = new boolean[V];
+        // boolean pv[] = new boolean[V];
+        // Stack<Integer> stack = new Stack();
 
-        for (int i = 0; i < V; i++) {
-            if (v[i] == false) {
-                if (dfs(i, map, v, pv, stack) == true) return new int[] {};
+        // for (int i = 0; i < V; i++) {
+        //     if (v[i] == false) {
+        //         if (dfs(i, map, v, pv, stack) == true) return new int[] {};
+        //     }
+        // }
+
+        // int res[] = new int[V];
+        // int i = 0;
+        // while (!stack.isEmpty()) {
+        //     res[i++] = stack.pop();
+        // }
+
+        // return res;
+
+        Queue<Integer> q = new LinkedList();
+        for(int i=0; i<V; i++) if(indegree[i] == 0) q.add(i);
+
+        int[] res = new int[V];
+        int i=0;
+
+        while(!q.isEmpty()){
+            int u = q.poll();
+            res[i++] = u;
+
+            for(int v: map.getOrDefault(u, new ArrayList<Integer>())){
+                indegree[v]--;
+                if(indegree[v] == 0) q.add(v);
             }
         }
 
-        int res[] = new int[V];
-        int i = 0;
-        while (!stack.isEmpty()) {
-            res[i++] = stack.pop();
-        }
+        return (i==V) ? res : new int[]{};
 
-        return res;
+        
     }
 
     public boolean dfs(int u, Map<Integer, List<Integer>> map, boolean visited[], boolean pv[], Stack<Integer> stack) {
