0404518c7b7b0af1d78d3003388c5af385c6ed43
mhk97 <149612134+mhk97@users.noreply.github.com>
2024-11-25 23:39:54 +0530

    Time: 5 ms (78.27%), Space: 45.8 MB (61.84%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -10,58 +10,67 @@ class Solution {
             map.computeIfAbsent(u, k -> new ArrayList()).add(v);
         }
 
-        //dfs based cycle find
-        // boolean visited[] = new boolean[V];
-        // boolean pv[] = new boolean[V];
+        // dfs based cycle find
+        boolean visited[] = new boolean[V];
+        boolean pv[] = new boolean[V];
+        Stack<Integer> stack = new Stack();
 
-        // for(int i=0; i<V; i++){
-        //     if(visited[i] == false){
-        //         if(dfs(i, visited, pv, map) == true) return false;
-        //     }
-        // }
-
-        //return true;
-
-        //Q based topo using kahnes bfs
-        Queue<Integer> q = new LinkedList();
-        for (int i = 0; i < V; i++) if (indegree[i] == 0) {
-            q.add(i);
-        }
-
-        List<Integer> list = new ArrayList();
-        while (!q.isEmpty()) {
-            int u = q.poll();
-            list.add(u);
-            for (int v : map.getOrDefault(u, new ArrayList<Integer>())) {
-                indegree[v]--;
-                if (indegree[v] == 0) {
-                    q.add(v);
-                }
+        for(int i=0; i<V; i++){
+            if(visited[i] == false){
+                if(dfs(i, visited, pv, map, stack) == true) return new int[]{};
             }
         }
 
-        if(V != list.size()) return new int[]{};
-
         int res[] = new int[V];
         int ind = 0;
-        for(int i: list){
-            res[ind++] = i;
+
+        while(!stack.isEmpty()){
+            res[ind++] = stack.pop();
         }
 
         return res;
+
+        //Q based topo using kahnes bfs
+        // Queue<Integer> q = new LinkedList();
+        // for (int i = 0; i < V; i++) if (indegree[i] == 0) {
+        //     q.add(i);
+        // }
+
+        // List<Integer> list = new ArrayList();
+        // while (!q.isEmpty()) {
+        //     int u = q.poll();
+        //     list.add(u);
+        //     for (int v : map.getOrDefault(u, new ArrayList<Integer>())) {
+        //         indegree[v]--;
+        //         if (indegree[v] == 0) {
+        //             q.add(v);
+        //         }
+        //     }
+        // }
+
+        // if(V != list.size()) return new int[]{};
+
+        // int res[] = new int[V];
+        // int ind = 0;
+        // for(int i: list){
+        //     res[ind++] = i;
+        // }
+
+        // return res;
     }
 
-    private boolean dfs(int u, boolean[] visited, boolean[] pv, Map<Integer, List<Integer>> map) {
+    private boolean dfs(int u, boolean[] visited, boolean[] pv, Map<Integer, List<Integer>> map, Stack<Integer> stack) {
         visited[u] = true;
         pv[u] = true;
 
         for (int v : map.getOrDefault(u, new ArrayList<Integer>())) {
             if (visited[v] == false) {
-                if (dfs(v, visited, pv, map) == true) return true;
+                if (dfs(v, visited, pv, map, stack) == true) return true;
             } else if (pv[v] == true) return true;
         }
 
         pv[u] = false;
+        stack.push(u);
         return false;
     }
 }
