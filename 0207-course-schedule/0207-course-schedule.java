6105c01398bd42e3dd4567f4f4e29aec59baade7
mhk97 <149612134+mhk97@users.noreply.github.com>
2024-11-25 23:17:03 +0530

    Time: 5 ms (79.99%), Space: 45.1 MB (71.4%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,31 +1,36 @@
 class Solution {
-
     public boolean canFinish(int V, int[][] edges) {
-        int indegree[] = new int[V];
-
-        //create a map
+        //create adj
         Map<Integer, List<Integer>> map = new HashMap();
-        for (int[] edge : edges) {
-            int u = edge[1];
-            int v = edge[0];
-            indegree[v]++;
-            map.computeIfAbsent(u, k -> new ArrayList()).add(v);
+        for(int[] edge: edges){
+            int u = edge[1], v = edge[0];
+            map.computeIfAbsent(u, k-> new ArrayList()).add(v);
         }
 
-        Queue<Integer> q = new LinkedList();
-        for (int i = 0; i < V; i++) if (indegree[i] == 0) q.add(i);
+        //dfs based cycle find
+        boolean visited[] = new boolean[V];
+        boolean pv[] = new boolean[V];
 
-        List<Integer> res = new ArrayList();
-
-        while (!q.isEmpty()) {
-            int u = q.poll();
-            res.add(u);
-            for (int v : map.getOrDefault(u, new ArrayList<Integer>())) {
-                indegree[v]--;
-                if (indegree[v] == 0) q.add(v);
+        for(int i=0; i<V; i++){
+            if(visited[i] == false){
+                if(dfs(i, visited, pv, map) == true) return false;
             }
         }
 
-        return res.size() == V;
+        return true;
+    }
+
+    private boolean dfs(int u, boolean[] visited, boolean[] pv, Map<Integer, List<Integer>> map){
+        visited[u] = true;
+        pv[u] = true;
+
+        for(int v: map.getOrDefault(u, new ArrayList<Integer>())){
+            if(visited[v] == false){
+                if(dfs(v, visited, pv, map) == true) return true;
+            }else if(pv[v] == true) return true;
+        }
+
+        pv[u] = false;
+        return false;
     }
 }
