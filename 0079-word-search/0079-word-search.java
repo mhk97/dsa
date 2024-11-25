fa602fd449fda4e9e474e0bb437c2a9ca458361e
mhk97 <149612134+mhk97@users.noreply.github.com>
2024-11-26 02:04:24 +0530

    Time: 207 ms (23.57%), Space: 41.7 MB (41.13%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,5 +1,4 @@
 class Solution {
-
     int[][] dirs = new int[][] { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
 
     public boolean exist(char[][] board, String word) {
@@ -8,8 +7,7 @@ class Solution {
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (board[i][j] == word.charAt(0)) {
-                    if (dfs(board, i, j, word, 0) == true)
-                        return true;
+                    if (dfs(board, i, j, word, 0) == true) return true;
                 }
             }
         }
@@ -17,18 +15,17 @@ class Solution {
         return false;
     }
 
-    public boolean dfs(char[][] board, int r, int c, String word, int ind) {
-        if (ind >= word.length())
-            return true;
-        if (r < 0 || c < 0 || r >= board.length || c >= board[0].length || board[r][c] != word.charAt(ind))
-            return false;
+    private boolean dfs(char[][] board, int r, int c, String word, int wi) {
+        if (wi >= word.length()) return true;
+
+        if (r < 0 || r >= board.length || c < 0 || c >= board[0].length || board[r][c] != word.charAt(wi)) return false;
 
         board[r][c] = '#';
-        for(int d[] : dirs){
-            int nr = d[0]+r, nc = d[1]+c;
-            if(dfs(board, nr, nc, word, ind+1) == true) return true;
+        for (int d[] : dirs) {
+            int nr = r + d[0], nc = c + d[1];
+            if (dfs(board, nr, nc, word, wi + 1) == true) return true;
         }
-        board[r][c] = word.charAt(ind);
+        board[r][c] = word.charAt(wi);
         return false;
     }
 }
