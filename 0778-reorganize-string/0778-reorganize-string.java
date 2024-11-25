7c7f7b2db7aebe704b647671e66771afc6f9f0ab
mhk97 <149612134+mhk97@users.noreply.github.com>
2024-11-25 23:57:05 +0530

    Time: 3 ms (71.54%), Space: 41.5 MB (76.24%) - LeetHub

--- 0778-reorganize-string/0778-reorganize-string.java
+++ 0778-reorganize-string/0778-reorganize-string.java
@@ -2,24 +2,30 @@ class Solution {
 
     public String reorganizeString(String s) {
         int[] arr = new int[26];
-        for (char c : s.toCharArray()) arr[c - 'a']++;
+        for (char c : s.toCharArray()) {
+            arr[c - 'a']++;
+        }
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> Integer.compare(b[0], a[0]));
+
+        for(int i=0; i< 26; i++){
+            if(arr[i] > 0) pq.offer(new int[]{arr[i], (i+'a')});
+        }
 
         StringBuilder sb = new StringBuilder();
-        PriorityQueue<int[]> pq = new PriorityQueue<int[]>((a, b) -> Integer.compare(b[0], a[0]));
 
-        for (int i = 0; i < arr.length; i++) if (arr[i] > 0) pq.offer(new int[] { arr[i], i + 'a' });
-
-        while (!pq.isEmpty()) {
+        while(!pq.isEmpty()){
             int[] first = pq.poll();
-            if (sb.length() == 0 || sb.charAt(sb.length() - 1) != ((char) first[1])) {
-                sb.append((char) first[1]); //append
-                if (--first[0] > 0) pq.offer(first);
-            } else {
-                if (pq.isEmpty()) return "";
-                int[] second = pq.poll();
-                sb.append((char) second[1]); //append
-
-                if (--second[0] > 0) pq.offer(second);
+            if(sb.length() == 0 || sb.charAt(sb.length()-1) !=  (char) first[1]){
+                sb.append((char)first[1]);
+                first[0]--;
+                if(first[0] > 0) pq.offer(first);
+            }else{
+                if(pq.isEmpty()) return "";
+                int[] sec = pq.poll();
+                sb.append((char)sec[1]);
+                sec[0]--;
+                if(sec[0] > 0) pq.offer(sec);
 
                 pq.offer(first);
             }
