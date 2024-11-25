5f455fca8573375cf7f0201f2218112d1884cc44
mhk97 <149612134+mhk97@users.noreply.github.com>
2024-11-25 23:32:48 +0530

    Time: 9 ms (27.87%), Space: 45.9 MB (15.49%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,33 +1,56 @@
 class Solution {
+
     public boolean canFinish(int V, int[][] edges) {
         //create adj
+        int[] indegree = new int[V];
         Map<Integer, List<Integer>> map = new HashMap();
-        for(int[] edge: edges){
+        for (int[] edge : edges) {
             int u = edge[1], v = edge[0];
-            map.computeIfAbsent(u, k-> new ArrayList()).add(v);
+            indegree[v]++;
+            map.computeIfAbsent(u, k -> new ArrayList()).add(v);
         }
 
         //dfs based cycle find
-        boolean visited[] = new boolean[V];
-        boolean pv[] = new boolean[V];
+        // boolean visited[] = new boolean[V];
+        // boolean pv[] = new boolean[V];
 
-        for(int i=0; i<V; i++){
-            if(visited[i] == false){
-                if(dfs(i, visited, pv, map) == true) return false;
+        // for(int i=0; i<V; i++){
+        //     if(visited[i] == false){
+        //         if(dfs(i, visited, pv, map) == true) return false;
+        //     }
+        // }
+
+        //return true;
+
+        //Q based topo using kahnes bfs
+        Queue<Integer> q = new LinkedList();
+        for (int i = 0; i < V; i++) if (indegree[i] == 0) {
+            q.add(i);
+        }
+        
+        List<Integer> list = new ArrayList();
+        while(!q.isEmpty()){
+            int u = q.poll();
+            list.add(u);
+            for(int v: map.getOrDefault(u, new ArrayList<Integer>())){
+                indegree[v]--;
+                if(indegree[v] == 0){
+                    q.add(v);
+                }
             }
         }
 
-        return true;
+        return list.size() == V;
     }
 
-    private boolean dfs(int u, boolean[] visited, boolean[] pv, Map<Integer, List<Integer>> map){
+    private boolean dfs(int u, boolean[] visited, boolean[] pv, Map<Integer, List<Integer>> map) {
         visited[u] = true;
         pv[u] = true;
 
-        for(int v: map.getOrDefault(u, new ArrayList<Integer>())){
-            if(visited[v] == false){
-                if(dfs(v, visited, pv, map) == true) return true;
-            }else if(pv[v] == true) return true;
+        for (int v : map.getOrDefault(u, new ArrayList<Integer>())) {
+            if (visited[v] == false) {
+                if (dfs(v, visited, pv, map) == true) return true;
+            } else if (pv[v] == true) return true;
         }
 
         pv[u] = false;
