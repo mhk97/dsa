36528697edc8462c094959ae7a7416fd12d68fdc
mhk97 <149612134+mhk97@users.noreply.github.com>
2024-12-04 01:32:36 +0530

    Time: 0 ms (100%), Space: 40.8 MB (13.35%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,25 +1,27 @@
 class Solution {
     int[][] dirs = new int[][] { { 1, 0 }, { 0, 1 } };
-    Integer cache[][] = new Integer[101][101];
+    Integer[][] cache;
 
     public int uniquePaths(int m, int n) {
-        return solve(m, n, 0,0);
+        cache = new Integer[m + 1][n + 1];
+        return solve(m, n, 0, 0);
     }
 
     public int solve(int m, int n, int i, int j) {
-        if(i==m-1 && j==n-1){
+        if (i == m - 1 && j == n - 1) {
             return 1;
         }
 
-        if(i<0 || j< 0 || i>=m || j>=n) return 0; 
+        if (i < 0 || i >= m || j < 0 || j >= n) return 0;
 
-        if(cache[i][j]!=null) return cache[i][j];
-
-        int res = 0;
-        for(int d[]: dirs){
-            int nr = d[0]+i, nc = d[1]+j;
-            res+=solve(m, n, nr, nc);
+        if (cache[i][j] != null) return cache[i][j];
+        
+        int curr = 0;
+        for (int d[] : dirs) {
+            int nr = d[0] + i, nc = d[1] + j;
+            curr += solve(m, n, nr, nc);
         }
-        return cache[i][j]= res;
+
+        return cache[i][j] = curr;
     }
 }
