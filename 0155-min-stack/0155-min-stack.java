a1f6bf9babac50d4b03a10d7d7ef052097f727b4
mhk97 <149612134+mhk97@users.noreply.github.com>
2024-12-04 14:29:52 +0530

    Time: 4 ms (96.78%), Space: 44.8 MB (49.22%) - LeetHub

--- 0155-min-stack/0155-min-stack.java
+++ 0155-min-stack/0155-min-stack.java
@@ -1,6 +1,5 @@
 class MinStack {
-    Stack<Integer> stack;
-    int min = Integer.MAX_VALUE;
+    Stack<int[]> stack;
 
     public MinStack() {
         stack = new Stack();
@@ -8,31 +7,24 @@ class MinStack {
 
     public void push(int val) {
         if (stack.isEmpty()) {
-            min = val;
-            stack.push(val);
-        } else if (val < min) {
-            stack.push(2 * val - min);
-            min = val;
-        } else {
-            stack.push(val);
+            stack.push(new int[] { val, val });
+            return;
         }
+        stack.push(new int[] { val, Math.min(val, stack.peek()[1]) });
     }
 
     public void pop() {
-        if (stack.isEmpty()) return;
-        if (stack.peek() < min) {
-            int curr = stack.pop();
-            min = 2 * min - curr;
-        } else stack.pop();
+        if (!stack.isEmpty()) {
+            stack.pop();
+        }
     }
 
     public int top() {
-        if (stack.isEmpty()) return -1;
-        if (stack.peek() < min) return min; else return stack.peek();
+        return stack.peek()[0];
     }
 
     public int getMin() {
-        return min;
+        return stack.peek()[1];
     }
 }
 /**
