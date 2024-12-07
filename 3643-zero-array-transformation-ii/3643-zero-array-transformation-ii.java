c240aa10226ea4b9223133df1c95f476de0f3f73
mhk97 <149612134+mhk97@users.noreply.github.com>
2024-12-07 13:44:11 +0530

    Time: 27 ms (44.96%), Space: 124.3 MB (12.42%) - LeetHub

--- 3643-zero-array-transformation-ii/3643-zero-array-transformation-ii.java
+++ 3643-zero-array-transformation-ii/3643-zero-array-transformation-ii.java
@@ -1,37 +1,34 @@
 class Solution {
 
     public int minZeroArray(int[] nums, int[][] q) {
-        int l = 0, h = q.length, min = Integer.MAX_VALUE;
+        int l = 0, h = q.length, res = Integer.MAX_VALUE;
 
         while (l <= h) {
             int mid = l + (h - l) / 2;
-            boolean res = check(nums, q, mid);
-            if (res == true) {
-                min = Math.min(min, mid);
+            if (isZero(nums, q, mid) == true) {
+                res = Math.min(res, mid);
                 h = mid - 1;
             } else {
                 l = mid + 1;
             }
         }
 
-        return min == Integer.MAX_VALUE ? -1 : min;
+        return res == Integer.MAX_VALUE ? -1 : res;
     }
 
-    public boolean check(int nums[], int[][] q, int index) {
+    public boolean isZero(int[] nums, int[][] q, int mid) {
         int[] prefix = new int[nums.length];
-
-        for (int k = 0; k < index; k++) {
-            int[] i = q[k];
-            int l = i[0], r = i[1] + 1;
-            prefix[l] += i[2];
-            if (r < prefix.length) prefix[r] -= i[2];
+        for (int i = 0; i < mid; i++) {
+            int l = q[i][0], r = q[i][1] + 1, val = q[i][2];
+            prefix[l] += val;
+            if (r < nums.length) prefix[r] -= val;
         }
 
         if (nums[0] - prefix[0] > 0) return false;
 
-        for (int i = 1; i < prefix.length; i++) {
+        for (int i = 1; i < nums.length; i++) {
             prefix[i] = prefix[i] + prefix[i - 1];
-            if (nums[i] - prefix[i] > 0) return false;
+            if (nums[i] - prefix[i] >0) return false;
         }
 
         return true;
