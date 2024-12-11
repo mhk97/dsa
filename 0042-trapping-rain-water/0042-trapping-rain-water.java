1269043f7b4b9760e10b2f800b140f4bb528267f
mhk97 <149612134+mhk97@users.noreply.github.com>
2024-12-11 21:08:20 +0530

    Time: 1 ms (65.41%), Space: 46.3 MB (52.88%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,29 +1,19 @@
 class Solution {
 
-    public int trap(int[] input) {
-        int n = input.length;
-        int lMax[] = new int[n];
-        int[] rMax = new int[n];
+    public int trap(int[] h) {
+        int leftMax[] = new int[h.length];
 
-        lMax[0] = 0;
-        for (int i = 1; i < n; i++) {
-            lMax[i] = Math.max(input[i - 1], lMax[i - 1]);
+        leftMax[0] = 0;
+        for (int i = 1; i < h.length; i++) {
+            leftMax[i] = Math.max(leftMax[i - 1], h[i - 1]);
         }
 
-        rMax[n - 1] = 0;
-        for (int i = n - 2; i >= 0; i--) {
-            rMax[i] = Math.max(input[i + 1], rMax[i + 1]);
+        int curr = 0, res = 0;
+        for (int i = h.length - 2; i >= 0; i--) {
+            curr = Math.max(h[i + 1], curr);
+            int x = Math.min(curr, leftMax[i]);
+            if (x > h[i]) res += x - h[i];
         }
-
-        int res = 0;
-
-        for(int i=0; i< n; i++){
-            int currMin = Math.min(lMax[i], rMax[i]);
-            int curr = currMin - input[i];
-            if(curr>0) res+=curr;
-        }
-
-
 
         return res;
     }
