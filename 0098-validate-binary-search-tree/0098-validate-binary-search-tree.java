2050cc90cc3cf9732f11007c684cc1d62992c0b2
mhk97 <149612134+mhk97@users.noreply.github.com>
2024-12-13 01:18:10 +0530

    Time: 0 ms (100%), Space: 44.9 MB (5.31%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -22,7 +22,8 @@ class Solution {
     public boolean solve(TreeNode root, Integer low, Integer high) {
         if (root == null) return true;
 
-        if ((low != null && root.val <= low) || (high != null && root.val >= high)) return false;
+        if ((low!= null && root.val <= low) ||
+         (high != null && root.val >= high)) return false;
 
         boolean left = solve(root.left, low, root.val);
         boolean right = solve(root.right, root.val, high);
