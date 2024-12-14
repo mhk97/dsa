7ea5d26940760cf69a6e1a4a6d84662a293dd00c
mhk97 <149612134+mhk97@users.noreply.github.com>
2024-12-15 00:48:10 +0530

    Time: 31 ms (34.25%), Space: 73.8 MB (34.03%) - LeetHub

--- 2176-parallel-courses-iii/2176-parallel-courses-iii.java
+++ 2176-parallel-courses-iii/2176-parallel-courses-iii.java
@@ -1,18 +1,16 @@
 class Solution {
 
     public int minimumTime(int n, int[][] relations, int[] time) {
-        Map<Integer, List<Integer>> map = new HashMap();
         int indegree[] = new int[n];
-
+        Map<Integer, List<Integer>> map = new HashMap();
         for (int edge[] : relations) {
             int u = edge[0] - 1, v = edge[1] - 1;
             map.computeIfAbsent(u, k -> new ArrayList()).add(v);
             indegree[v]++;
         }
 
-        int[] max = new int[n];
         Queue<Integer> q = new LinkedList();
-
+        int max[] = new int[n];
         for (int i = 0; i < n; i++) {
             if (indegree[i] == 0) {
                 q.add(i);
@@ -20,20 +18,21 @@ class Solution {
             }
         }
 
-        int res = 0;
-
         while (!q.isEmpty()) {
-            int u = q.poll();
-            for (int v : map.getOrDefault(u, new ArrayList<Integer>())) {
-                indegree[v]--;
-                int vCost = time[v] + max[u];
-                max[v] = Math.max(vCost, max[v]);
-                if (indegree[v] == 0) q.add(v);
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                int u = q.poll();
+                for (int v : map.getOrDefault(u, new ArrayList<Integer>())) {
+                    int cost = max[u] + time[v];
+                    max[v] = Math.max(cost, max[v]);
+                    indegree[v]--;
+                    if (indegree[v] == 0) q.add(v);
+                }
             }
         }
 
-        for (int i : max) res = Math.max(res, i);
-
+        int res = 0;
+        for (int i : max) res = Math.max(i, res);
         return res;
     }
 }
