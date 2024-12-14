95606911d7d563cde6c949b220bef67c7dcd9037
mhk97 <149612134+mhk97@users.noreply.github.com>
2024-12-14 23:17:29 +0530

    Time: 6 ms (30.22%), Space: 46.2 MB (43.38%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,12 +1,24 @@
 class Solution {
 
-    public int singleNumber(int[] nums) {
-        int res = nums[0];
+    public int singleNumber(int[] ints) {
+        Arrays.sort(ints);
 
-        for (int i = 1; i < nums.length; i++) {
-            res = nums[i] ^ res;
+        int l = 0, r = ints.length - 1;
+        int res = -1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (l == r) {
+                res = ints[l];
+                break;
+            }
+            if (mid % 2 != 0) mid--;
+
+            if (ints[mid] == ints[mid + 1]) {
+                l = mid + 2;
+            } else {
+                r = mid;
+            }
         }
-
         return res;
     }
 }
