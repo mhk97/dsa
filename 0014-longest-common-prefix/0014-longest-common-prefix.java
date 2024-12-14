800c3bfbfab736788a1832ff480374207793cf0d
mhk97 <149612134+mhk97@users.noreply.github.com>
2024-12-14 22:23:46 +0530

    Time: 2 ms (22.14%), Space: 42 MB (18.65%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,18 +1,75 @@
-class Solution {
+class Node {
+    Node links[] = new Node[26];
+    boolean isEnd = false;
 
-    public String longestCommonPrefix(String[] strs) {
-        StringBuilder sb = new StringBuilder();
+    public void setEnd() {
+        isEnd = true;
+    }
 
-        String temp = strs[0];
-        for (int i = 0; i < temp.length(); i++) {
-            char t = temp.charAt(i);
-            for (int j = 1; j < strs.length; j++) {
-                String t2 = strs[j];
-                if (i>= t2.length()||t2.charAt(i) != t) return sb.toString();
-            }
-            sb.append(t);
+    public boolean isEnd() {
+        return isEnd;
+    }
+
+    public void put(Node n, char c) {
+        links[c - 'a'] = n;
+    }
+
+    public Node get(char c) {
+        return links[c - 'a'];
+    }
+
+    public boolean contains(char c) {
+        return links[c - 'a'] != null;
+    }
+
+    public int countChar() {
+        int count = 0;
+
+        for (Node n : links) {
+            if (n != null) count++;
         }
 
-        return sb.toString();
+        return count;
+    }
+}
+
+class Solution {
+    Node root = new Node();
+
+    public void insert(String s) {
+        Node node = root;
+
+        for (char c : s.toCharArray()) {
+            if (!node.contains(c)) {
+                node.put(new Node(), c);
+            }
+            node = node.get(c);
+        }
+
+        node.setEnd();
+    }
+
+    public String longest(String s) {
+        Node node = root;
+        int i = 0;
+        for (char c : s.toCharArray()) {
+            if (node.contains(c) && node.countChar() == 1 && node.isEnd() == false) {
+                i++;
+                node = node.get(c);
+            } else break;
+        }
+
+        return s.substring(0, i);
+    }
+
+    public String longestCommonPrefix(String[] strs) {
+        String min = strs[0];
+
+        for (String s : strs) {
+            if (min.length() > s.length()) min = s;
+            insert(s);
+        }
+
+        return longest(min);
     }
 }
