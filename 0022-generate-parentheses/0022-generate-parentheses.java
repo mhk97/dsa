a7b3a270429cb4145f0b7d4982d315d9b7ea180b
mhk97 <149612134+mhk97@users.noreply.github.com>
2024-12-15 22:11:00 +0530

    Time: 0 ms (100%), Space: 42.7 MB (86.73%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -2,24 +2,31 @@ class Solution {
     List<String> res = new ArrayList();
 
     public List<String> generateParenthesis(int n) {
-        solve(new StringBuilder(), n, n);
+        solve(n, n, new StringBuilder());
         return res;
     }
 
-    public void solve(StringBuilder sb, int o, int c) {
+    public void solve(int o, int c, StringBuilder sb) {
         if (o == 0 && c == 0) {
             res.add(sb.toString());
+            return;
         }
 
-        if (o != 0) {
+        if (o < 0 || c < 0) {
+            return;
+        }
+
+        if (o == c) {
             sb.append('(');
-            solve(sb, o - 1, c);
+            solve(o - 1, c, sb);
+            sb.deleteCharAt(sb.length()-1);
+        } else {
+            sb.append('(');
+            solve(o - 1, c, sb);
             sb.deleteCharAt(sb.length() - 1);
-        }
 
-        if (o < c) {
             sb.append(')');
-            solve(sb, o, c - 1);
+            solve(o, c - 1, sb);
             sb.deleteCharAt(sb.length() - 1);
         }
     }
