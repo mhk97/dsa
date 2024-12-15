52b9716fc2fdb2409cc40535c8ecc1a4e33d086c
mhk97 <149612134+mhk97@users.noreply.github.com>
2024-12-15 08:25:44 +0530

    Time: 1 ms (99.39%), Space: 57 MB (59.5%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,16 +1,16 @@
 class Solution {
+
+    //kadanes
     public int maxSubArray(int[] nums) {
-        int max = nums[0], currSum = nums[0];
-
-        for(int i=1; i<nums.length; i++){
-            if(currSum<0) {
-                currSum = 0;
+        int sum = nums[0], res = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (sum < 0) {
+                sum = 0;
             }
-
-            currSum +=nums[i];
-            max = Math.max(max, currSum);
+            sum += nums[i];
+            res = Math.max(res, sum);
         }
 
-        return max;
+        return res;
     }
 }
