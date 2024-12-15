e362f9f17679b2fdaa10b13c1c62601782604f8d
mhk97 <149612134+mhk97@users.noreply.github.com>
2024-12-15 18:05:01 +0530

    Time: 0 ms (100%), Space: 42.4 MB (16.5%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
@@ -14,24 +14,23 @@
  * }
  */
 class Solution {
+
     public void flatten(TreeNode root) {
-        flattenTree(root);
+        solve(root);
     }
 
-    public TreeNode flattenTree(TreeNode root){
-        if(root == null) return root;
+    public TreeNode solve(TreeNode root) {
+        if (root == null || (root.left == null && root.right == null)) return root;
 
-        if(root.left == null && root.right == null) return root;
+        TreeNode left = solve(root.left);
+        TreeNode right = solve(root.right);
 
-        TreeNode left = flattenTree(root.left);
-        TreeNode right = flattenTree(root.right);
-
-        if(left!=null){
+        if (left != null) {
             left.right = root.right;
             root.right = root.left;
             root.left = null;
         }
 
-        return right == null ? left : right;
+        return (right != null) ? right : left;
     }
 }
