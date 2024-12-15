d894b1f1cf6aa5a07d99f960be22c3afd10475ce
mhk97 <149612134+mhk97@users.noreply.github.com>
2024-12-15 15:43:36 +0530

    Time: 13 ms (9.04%), Space: 44.8 MB (22.74%) - LeetHub

--- 1085-the-earliest-moment-when-everyone-become-friends/1085-the-earliest-moment-when-everyone-become-friends.java
+++ 1085-the-earliest-moment-when-everyone-become-friends/1085-the-earliest-moment-when-everyone-become-friends.java
@@ -1,47 +1,41 @@
 class Solution {
 
     public int earliestAcq(int[][] logs, int n) {
-        Map<Integer, List<Pair<Integer, Integer>>> map = new HashMap();
-
-        for (int edge[] : logs) {
-            int cost = edge[0];
-            int u = edge[1];
-            int v = edge[2];
-
-            map.computeIfAbsent(u, k -> new ArrayList()).add(new Pair(v, cost));
-            map.computeIfAbsent(v, k -> new ArrayList()).add(new Pair(u, cost));
-        }
-
         PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> Integer.compare(a[0], b[0]));
+        Map<Integer, List<int[]>> map = new HashMap();
+
+        for (int[] edge : logs) {
+            int cost = edge[0], u = edge[1], v = edge[2];
+            map.computeIfAbsent(u, k -> new ArrayList()).add(new int[] { cost, v });
+            map.computeIfAbsent(v, k -> new ArrayList()).add(new int[] { cost, u });
+        }
 
         boolean visited[] = new boolean[n];
 
+        pq.add(new int[] { -1, 0 });
         int res = Integer.MIN_VALUE;
-
-        pq.offer(new int[] { 0, 0, -1 });
 
         while (!pq.isEmpty()) {
             int curr[] = pq.poll();
+            int cost = curr[0], u = curr[1];
 
-            int cost = curr[0], v = curr[1], u = curr[0];
-
-            if (visited[v] == false) {
-                visited[v] = true;
-                res = Math.max(cost, res);
-
-                for (Pair<Integer, Integer> pair : map.getOrDefault(v, new ArrayList<>())) {
-                    int next = pair.getKey();
-                    if (visited[next] == false) {
-                        pq.offer(new int[] { pair.getValue(), next, v });
+            if (visited[u] == false) {
+                visited[u] = true;
+                res = Math.max(res, cost);
+                for (int v[] : map.getOrDefault(u, new ArrayList<int[]>())) {
+                    int newCost = v[0];
+                    int nextNode = v[1];
+                    if (visited[nextNode] == false) {
+                        pq.add(v);
                     }
                 }
             }
         }
 
-        boolean flag = true;
+        for (boolean a : visited) {
+            if (a == false) return -1;
+        }
 
-        for (boolean i : visited) if (i == false) flag = false;
-
-        return (flag) ? res : -1;
+        return res;
     }
 }
