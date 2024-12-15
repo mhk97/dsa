1e265e5f86c6fad58b5304bf7c358372f3e58afc
mhk97 <149612134+mhk97@users.noreply.github.com>
2024-12-15 20:10:56 +0530

    Time: 5 ms (80.02%), Space: 45 MB (81.16%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,33 +1,31 @@
 class Solution {
-
-    public boolean canFinish(int n, int[][] p) {
+    public boolean canFinish(int V, int[][] edges) {
         Map<Integer, List<Integer>> map = new HashMap();
+        boolean visited[] = new boolean[V];
+        boolean pv[] = new boolean[V];
 
-        for (int[] i : p) {
-            int u = i[1], v = i[0];
-            map.computeIfAbsent(u, k -> new ArrayList()).add(v);
+        for(int edge[]: edges){
+            int u = edge[1], v = edge[0];
+            map.computeIfAbsent(u, k-> new ArrayList()).add(v);
         }
 
-        boolean[] visited = new boolean[n];
-        boolean[] pathVis = new boolean[n];
-
-        for (int u = 0; u < n; u++) {
-            if (visited[u] == false) {
-                if (isCycle(visited, pathVis, map, u) == true) return false;
+        for(int i=0; i<V; i++){
+            if(visited[i] == false){
+                if(cycle(visited, pv, map, i) == true) return false;
             }
         }
 
         return true;
     }
 
-    public boolean isCycle(boolean visited[], boolean pv[], Map<Integer, List<Integer>> map, int u) {
+    public boolean cycle(boolean visited[], boolean pv[], Map<Integer, List<Integer>> map, int u){
         visited[u] = true;
         pv[u] = true;
 
-        for (int v : map.getOrDefault(u, new ArrayList<Integer>())) {
-            if (visited[v] == false) {
-                if (isCycle(visited, pv, map, v) == true) return true;
-            } else if (pv[v] == true) return true;
+        for(int v: map.getOrDefault(u, new ArrayList<Integer>())){
+            if(visited[v] == false){
+                if(cycle(visited, pv, map, v)== true) return true;
+            }else if(pv[v] == true) return true;
         }
 
         pv[u] = false;
