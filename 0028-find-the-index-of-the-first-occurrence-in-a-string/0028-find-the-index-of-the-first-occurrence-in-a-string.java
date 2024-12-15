ffb66f62ec768925fc305a179e60764106217b19
mhk97 <149612134+mhk97@users.noreply.github.com>
2024-12-15 12:09:38 +0530

    Time: 2 ms (23.73%), Space: 41.4 MB (58.73%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,17 +1,20 @@
 class Solution {
 
-    public int[] findLps(String p) {
-        int[] lps = new int[p.length()];
-        int i = 1, n = p.length(), len = 0;
+    public int[] lps(String p) {
+        int n = p.length(), i = 1, len = 0;
+        int[] lps = new int[n];
+
         lps[0] = 0;
 
         while (i < n) {
-            if (p.charAt(i) == p.charAt(len)) {
+            if (p.charAt(len) == p.charAt(i)) {
                 len++;
                 lps[i] = len;
                 i++;
             } else {
-                if (len != 0) len = lps[len - 1]; else {
+                if (len != 0) {
+                    len = lps[len - 1];
+                } else {
                     lps[i] = 0;
                     i++;
                 }
@@ -21,20 +24,20 @@ class Solution {
         return lps;
     }
 
-    public int strStr(String a, String b) {
-        // System.out.println(Arrays.toString(findLps(b)));
-        int[] lps = findLps(b);
+    public int strStr(String s, String p) {
+        int lps[] = lps(p);
+        System.out.println(Arrays.toString(lps(p)));
 
-        int i = 0, j = 0, m = b.length(), n = a.length();
+        int i = 0, j = 0, slen = s.length(), plen = p.length();
 
-        while (i < n) {
-            if (j < m && a.charAt(i) == b.charAt(j)) {
+        while (i < slen) {
+            if (j < plen && s.charAt(i) == p.charAt(j)) {
                 i++;
                 j++;
             }
 
-            if (j >= m) return i - j; else if (i < n && (j == 0 || a.charAt(i) != b.charAt(j))) {
-                if (j != 0) j = lps[j - 1]; else i++;
+            if (j >= plen) return i - j; else if (i < slen && (s.charAt(i) != p.charAt(j))) {
+                if (j > 0) j = lps[j - 1]; else i++;
             }
         }
 
