90a3a6bbac706293e37e1b94481a611a2b860a45
mhk97 <149612134+mhk97@users.noreply.github.com>
2024-12-15 17:54:48 +0530

    Time: 48 ms (11.95%), Space: 45.1 MB (13.85%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,37 +1,37 @@
 class Solution {
 
     public int characterReplacement(String s, int k) {
-        int n = s.length(), i = 0, j = 0, max = 0, res = 0;
-
+        int i = 0, j = 0, res = 0, max = 0, n = s.length();
         Map<Character, Integer> map = new HashMap();
 
         while (j < n) {
-            char temp = s.charAt(j);
+            char curr = s.charAt(j);
 
-            int f = map.getOrDefault(temp, 0) + 1;
-            map.put(temp, f);
-            max = Math.max(max, f);
+            int count = map.getOrDefault(curr, 0) + 1;
+            map.put(curr, count);
 
-            if ((j - i + 1 - max) <= k) {
+            max = Math.max(max, count);
+
+            if (j - i + 1 - max <= k) {
                 res = Math.max(res, j - i + 1);
-            }
-
-            while ((j - i + 1 - max) > k) {
-                char it = s.charAt(i++);
-                int fi = map.getOrDefault(it, 0) - 1;
-                if (fi <= 0) map.remove(it);
-                map.put(it, fi);
-                max = findMax(map);
+            } else {
+                while (j - i + 1 - max > k) {
+                    char icurr = s.charAt(i++);
+                    int icount = map.getOrDefault(icurr, 0) - 1;
+                    if (icount <= 0) map.remove(icurr); else map.put(icurr, icount);
+                    max = findMax(map);
+                }
             }
 
             j++;
         }
+
         return res;
     }
 
     public int findMax(Map<Character, Integer> map) {
-        int res = 0;
-        for (int i : map.values()) res = Math.max(i, res);
-        return res;
+        int max = Integer.MIN_VALUE;
+        for (int i : map.values()) max = Math.max(i, max);
+        return max;
     }
 }
