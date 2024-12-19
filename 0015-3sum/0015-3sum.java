51175b73752e49e685d52de21884f011b67bed67
mhk97 <149612134+mhk97@users.noreply.github.com>
2024-12-20 05:03:22 +0530

    Time: 26 ms (92.19%), Space: 51.6 MB (44.92%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -5,21 +5,20 @@ class Solution {
 
         List<List<Integer>> res = new ArrayList();
 
-        for (int i = 0; i < nums.length - 2; i++) {
+        for (int i = 0; i < nums.length; i++) {
             int j = i + 1, k = nums.length - 1;
+
             if (i != 0 && nums[i] == nums[i - 1]) continue;
+
             while (j < k) {
                 int sum = nums[i] + nums[j] + nums[k];
-                if (sum > 0) {
-                    k--;
-                } else if (sum < 0) {
-                    j++;
-                } else {
-                    res.add(List.of(nums[i], nums[j], nums[k]));
+                if (sum < 0) j++; else if (sum > 0) k--; else {
+                    res.add(Arrays.asList(nums[i], nums[j], nums[k]));
                     j++;
                     k--;
+
                     while (j < k && nums[j] == nums[j - 1]) j++;
-                    while (j < k && nums[k] == nums[k + 1]) k--;
+                    while (j < k && nums[j] == nums[k + 1]) k--;
                 }
             }
         }
