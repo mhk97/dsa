414ef122a88f0a0206a02afc6e546855f3346ebe
mhk97 <149612134+mhk97@users.noreply.github.com>
2024-12-19 21:04:07 +0530

    Time: 6 ms (93.05%), Space: 42.1 MB (88.88%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,22 +1,26 @@
 class Solution {
 
-    public String create(String s) {
-        StringBuilder sb = new StringBuilder();
-        Map<Character, Integer> map = new HashMap();
+    public boolean isIsomorphic(String s, String t) {
+        int sarr[] = new int[256];
+        int tarr[] = new int[256];
+
+        Arrays.fill(sarr, -1);
+        Arrays.fill(tarr, -1);
+
+        if (s.length() != t.length()) return false;
 
         for (int i = 0; i < s.length(); i++) {
-            char temp = s.charAt(i);
+            char schar = s.charAt(i);
+            char tchar = t.charAt(i);
 
-            if (!map.containsKey(temp)) map.put(temp, i);
+            if (sarr[schar] == -1 && tarr[tchar] == -1) {
+                sarr[schar] = tchar;
+                tarr[tchar] = schar;
+            }
 
-            sb.append(Integer.toString(map.get(temp)));
-            sb.append("#");
+            if (sarr[schar] != tchar || tarr[tchar] != schar) return false;
         }
-        // System.out.println(sb.toS);
-        return sb.toString();
-    }
 
-    public boolean isIsomorphic(String s, String t) {
-        return create(s).equals(create(t));
+        return true;
     }
 }
