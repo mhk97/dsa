cc9157a96a1abaef873f8667a1ff537fec624b36
mhk97 <149612134+mhk97@users.noreply.github.com>
2024-12-20 06:54:08 +0530

    Time: 9 ms (17.78%), Space: 45.9 MB (57.17%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,32 +1,34 @@
 class Solution {
 
-    public int[] findOrder(int n, int[][] p) {
+    public int[] findOrder(int V, int[][] edges) {
         Map<Integer, List<Integer>> map = new HashMap();
-        int[] indegree = new int[n];
-        for (int i[] : p) {
-            int u = i[1], v = i[0];
+
+        int[] indegree = new int[V];
+
+        for (int edge[] : edges) {
+            int u = edge[1], v = edge[0];
+            map.computeIfAbsent(u, j -> new ArrayList()).add(v);
             indegree[v]++;
-            map.computeIfAbsent(u, k -> new ArrayList()).add(v);
         }
 
-        Queue<Integer> q = new LinkedList();
         List<Integer> res = new ArrayList();
-        for (int i = 0; i < n; i++) if (indegree[i] == 0) q.add(i);
+        int m = 0;
+        Queue<Integer> q = new LinkedList();
 
-        while(!q.isEmpty()){
+        for (int i = 0; i < V; i++) {
+            if (indegree[i] == 0) q.add(i);
+        }
+
+        while (!q.isEmpty()) {
             int u = q.poll();
             res.add(u);
-            for(int v: map.getOrDefault(u, new ArrayList<Integer>())){
+
+            for (int v : map.getOrDefault(u, new ArrayList<Integer>())) {
                 indegree[v]--;
-                if(indegree[v] == 0) q.add(v);
+                if (indegree[v] == 0) q.add(v);
             }
         }
 
-        int op[] = new int[n];
-        if(res.size() != n) return new int[]{};
-        for(int i=0; i< res.size(); i++) op[i] = res.get(i);
-
-        return op;
-
+        return res.size() != V ? new int[]{} : res.stream().mapToInt(Integer::intValue).toArray();
     }
 }
