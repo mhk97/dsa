6d160942bd253a8abe78c042318e960cfe30ae8e
mhk97 <149612134+mhk97@users.noreply.github.com>
2024-12-20 15:45:31 +0530

    Time: 0 ms (100%), Space: 41.7 MB (26.66%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -1,37 +1,39 @@
 class Solution {
 
-    public List<Integer> spiralOrder(int[][] matrix) {
-        int top = 0, left = 0, right = matrix[0].length - 1, bottom = matrix.length - 1;
+    public List<Integer> spiralOrder(int[][] m) {
+        int row = m.length, col = m[0].length;
+
+        int left = 0, top = 0, right = col - 1, bottom = row - 1;
 
         List<Integer> res = new ArrayList();
 
-        while (top <= bottom && left <= right) {
-            //r to l -  row const
+        while (left <= right && top <= bottom) {
+            //left to right
             for (int i = left; i <= right; i++) {
-                res.add(matrix[top][i]);
+                res.add(m[top][i]);
             }
 
             top++;
 
-            //top to bottom -- col const
+            //top to bottom
             for (int i = top; i <= bottom; i++) {
-                res.add(matrix[i][right]);
+                res.add(m[i][right]);
             }
 
             right--;
 
-            //right to left = row const
+            //right to left
             if (top <= bottom) {
                 for (int i = right; i >= left; i--) {
-                    res.add(matrix[bottom][i]);
+                    res.add(m[bottom][i]);
                 }
                 bottom--;
             }
 
-            //bottom to top --- col const
+            //bottom to top
             if (left <= right) {
                 for (int i = bottom; i >= top; i--) {
-                    res.add(matrix[i][left]);
+                    res.add(m[i][left]);
                 }
                 left++;
             }
