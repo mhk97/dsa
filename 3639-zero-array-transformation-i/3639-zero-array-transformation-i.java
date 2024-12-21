7d19ccbc5ff2b54883b953017ff3581e6c03f140
mhk97 <149612134+mhk97@users.noreply.github.com>
2024-12-21 13:56:17 +0530

    Time: 3 ms (88.92%), Space: 95.3 MB (56.13%) - LeetHub

--- 3639-zero-array-transformation-i/3639-zero-array-transformation-i.java
+++ 3639-zero-array-transformation-i/3639-zero-array-transformation-i.java
@@ -1,18 +1,19 @@
 class Solution {
 
     public boolean isZeroArray(int[] nums, int[][] q) {
-        int[] prefix = new int[nums.length];
-        for (int i[] : q) {
-            int l = i[0], r = i[1] + 1;
+        int prefix[] = new int[nums.length];
+
+        for (int k[] : q) {
+            int l = k[0], h = k[1] + 1;
+            if (h < nums.length) prefix[h]--;
             prefix[l]++;
-            if (r < nums.length) prefix[r]--;
         }
 
-        if (nums[0] > prefix[0]) return false;
+        if (nums[0] - prefix[0] > 0) return false;
 
         for (int i = 1; i < nums.length; i++) {
             prefix[i] = prefix[i] + prefix[i - 1];
-            if (prefix[i] < nums[i]) return false;
+            if (nums[i] - prefix[i] > 0) return false;
         }
 
         return true;
