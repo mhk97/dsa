14572a8551e0764aa6c4de00a9fc5e127e23e430
mhk97 <149612134+mhk97@users.noreply.github.com>
2024-12-22 00:42:34 +0530

    Time: 1066 ms (31.17%), Space: 65.8 MB (57.8%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,21 +1,23 @@
 class Solution {
 
     public int longestConsecutive(int[] nums) {
-        Set<Integer> set = new HashSet();
+        HashSet<Integer> set = new HashSet();
         for (int i : nums) set.add(i);
 
-        int res = 0;
-        for (int i : nums) {
-            int temp = i;
-            if (set.contains(temp - 1)) continue;
-            int cnt = 1;
-            while (set.contains(temp)) {
-                temp = i + cnt;
-                cnt++;
+        int max = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            int k = nums[i];
+            if (set.contains(k - 1)) continue;
+            int res = 0;
+            while (set.contains(k)) {
+                res += 1;
+                k += 1;
             }
-            res = Math.max(res, cnt - 1);
+
+            max = Math.max(max, res);
         }
 
-        return res;
+        return max;
     }
 }
