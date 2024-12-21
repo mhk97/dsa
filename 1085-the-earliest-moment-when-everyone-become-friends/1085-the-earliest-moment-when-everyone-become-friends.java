638aa804df8eebeae4eaddabb60f4f31cb7b5562
mhk97 <149612134+mhk97@users.noreply.github.com>
2024-12-21 19:25:54 +0530

    Time: 13 ms (9.8%), Space: 45.3 MB (5.99%) - LeetHub

--- 1085-the-earliest-moment-when-everyone-become-friends/1085-the-earliest-moment-when-everyone-become-friends.java
+++ 1085-the-earliest-moment-when-everyone-become-friends/1085-the-earliest-moment-when-everyone-become-friends.java
@@ -1,40 +1,36 @@
 class Solution {
 
     public int earliestAcq(int[][] logs, int n) {
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> Integer.compare(a[0], b[0]));
         Map<Integer, List<int[]>> map = new HashMap();
 
-        for (int[] edge : logs) {
-            int cost = edge[0], u = edge[1], v = edge[2];
-            map.computeIfAbsent(u, k -> new ArrayList()).add(new int[] { cost, v });
-            map.computeIfAbsent(v, k -> new ArrayList()).add(new int[] { cost, u });
+        for (int i[] : logs) {
+            int u = i[1], v = i[2], val = i[0];
+            map.computeIfAbsent(u, k -> new ArrayList()).add(new int[] { val, v });
+            map.computeIfAbsent(v, k -> new ArrayList()).add(new int[] { val, u });
         }
 
-        boolean visited[] = new boolean[n];
+        PriorityQueue<int[]> pq = new PriorityQueue<int[]>((a, b) -> Integer.compare(a[0], b[0]));
 
         pq.add(new int[] { -1, 0 });
+
         int res = Integer.MIN_VALUE;
+        boolean visited[] = new boolean[n];
 
         while (!pq.isEmpty()) {
-            int curr[] = pq.poll();
-            int cost = curr[0], u = curr[1];
-
-            if (visited[u] == false) {
-                visited[u] = true;
+            int[] temp = pq.poll();
+            int node = temp[1], cost = temp[0];
+            if (visited[node] == false) {
                 res = Math.max(res, cost);
-                for (int v[] : map.getOrDefault(u, new ArrayList<int[]>())) {
-                    int newCost = v[0];
-                    int nextNode = v[1];
-                    if (visited[nextNode] == false) {
+                visited[node] = true;
+                for (int v[] : map.getOrDefault(node, new ArrayList<int[]>())) {
+                    if (visited[v[1]] == false) {
                         pq.add(v);
                     }
                 }
             }
         }
 
-        for (boolean a : visited) {
-            if (a == false) return -1;
-        }
+        for(boolean flag: visited) if(flag == false) return -1;
 
         return res;
     }
