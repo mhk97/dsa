1ed11e3193f1863268d083b0d87cecfe6cd7ec30
mhk97 <149612134+mhk97@users.noreply.github.com>
2024-12-22 01:03:38 +0530

    Time: 0 ms (100%), Space: 41.1 MB (34.53%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,18 +1,18 @@
 class Solution {
 
+    Integer cache[];
     public int rob(int[] nums) {
-        Integer[] cache = new Integer[nums.length];
-        return solve(nums, 0, cache);
+        cache = new Integer[nums.length];
+        return solve(nums, 0);
     }
 
-    public int solve(int[] nums, int i, Integer[] cache) {
+    public int solve(int[] nums, int i) {
         if (i >= nums.length) return 0;
+        if(cache[i]!=null) return cache[i];
 
-        if (cache[i] != null) return cache[i];
+        int take = nums[i] + solve(nums, i + 2);
+        int skip = 0 + solve(nums, i + 1);
 
-        int take = nums[i] + solve(nums, i + 2, cache);
-        int skip = 0 + solve(nums, i + 1, cache);
-
-        return cache[i] = Math.max(take, skip);
+        return cache[i]= Math.max(take, skip);
     }
 }
