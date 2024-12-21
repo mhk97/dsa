54a24b139a504b2a77c1c71ce2d55e5dce8c4a69
mhk97 <149612134+mhk97@users.noreply.github.com>
2024-12-21 16:47:31 +0530

    Time: 6 ms (74.6%), Space: 44.4 MB (98.45%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,31 +1,33 @@
 class Solution {
+
     public boolean canFinish(int V, int[][] edges) {
         Map<Integer, List<Integer>> map = new HashMap();
+
+        for (int edge[] : edges) {
+            int u = edge[1], v = edge[0];
+            map.computeIfAbsent(u, j -> new ArrayList()).add(v);
+        }
+
         boolean visited[] = new boolean[V];
         boolean pv[] = new boolean[V];
 
-        for(int edge[]: edges){
-            int u = edge[1], v = edge[0];
-            map.computeIfAbsent(u, k-> new ArrayList()).add(v);
-        }
-
-        for(int i=0; i<V; i++){
-            if(visited[i] == false){
-                if(cycle(visited, pv, map, i) == true) return false;
+        for (int i = 0; i < V; i++) {
+            if (visited[i] == false) {
+                if (cycle(visited, pv, map, i) == true) return false;
             }
         }
 
         return true;
     }
 
-    public boolean cycle(boolean visited[], boolean pv[], Map<Integer, List<Integer>> map, int u){
+    public boolean cycle(boolean visited[], boolean pv[], Map<Integer, List<Integer>> map, int u) {
         visited[u] = true;
         pv[u] = true;
 
-        for(int v: map.getOrDefault(u, new ArrayList<Integer>())){
-            if(visited[v] == false){
-                if(cycle(visited, pv, map, v)== true) return true;
-            }else if(pv[v] == true) return true;
+        for (int v : map.getOrDefault(u, new ArrayList<Integer>())) {
+            if (visited[v] == false) {
+                if (cycle(visited, pv, map, v) == true) return true;
+            } else if (pv[v] == true) return true;
         }
 
         pv[u] = false;
