3b04fe0768bf52c0559f028809905e34ad7ef90a
mhk97 <149612134+mhk97@users.noreply.github.com>
2024-12-21 20:00:26 +0530

    Time: 8 ms (64.17%), Space: 46.4 MB (10.25%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.java
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.java
@@ -1,33 +1,18 @@
 class Solution {
 
     public int minMeetingRooms(int[][] intervals) {
-        int n = intervals.length;
-        int start[] = new int[n];
-        int end[] = new int[n];
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
 
-        int k = 0;
-        for (int m[] : intervals) {
-            start[k] = m[0];
-            end[k] = m[1];
-            k++;
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+
+        pq.offer(intervals[0][1]);
+
+        for (int i = 1; i < intervals.length; i++) {
+            int currStart = intervals[i][0], currEnd = intervals[i][1];
+            if (!pq.isEmpty() && pq.peek() <= currStart) pq.poll();
+            pq.offer(currEnd);
         }
 
-        Arrays.sort(start);
-        Arrays.sort(end);
-
-        int i = 0, j = 0, res = 0, max = Integer.MIN_VALUE;
-
-        while (i < n && j < n) {
-            if (start[i] < end[j]) {
-                res++;
-                i++;
-            } else {
-                j++;
-                res--;
-            }
-            max = Math.max(res, max);
-        }
-
-        return max;
+        return pq.size();
     }
 }
