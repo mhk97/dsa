69bd0896957aea27178fef7700248bb233fe5647
mhk97 <149612134+mhk97@users.noreply.github.com>
2024-12-21 16:09:44 +0530

    Time: 28 ms (40.02%), Space: 124.3 MB (14.05%) - LeetHub

--- 3643-zero-array-transformation-ii/3643-zero-array-transformation-ii.java
+++ 3643-zero-array-transformation-ii/3643-zero-array-transformation-ii.java
@@ -1,36 +1,36 @@
 class Solution {
 
-    public boolean canMakeZero(int nums[], int[][] queries, int mid) {
-        int[] prefix = new int[nums.length];
-        for (int i = 0; i < mid; i++) {
-            int q[] = queries[i];
-            int l = q[0], h = q[1] + 1, val = q[2];
-
-            if (h < nums.length) prefix[h] -= val;
-            prefix[l] += val;
-        }
-
-        if (nums[0] - prefix[0] > 0) return false;
-        for (int i = 1; i < prefix.length; i++) {
-            prefix[i] = prefix[i] + prefix[i - 1];
-            int diff = nums[i] - prefix[i];
-            if (diff > 0) return false;
-        }
-
-        return true;
-    }
-
-    public int minZeroArray(int[] nums, int[][] q) {
-        int l = 0, h = q.length, res = Integer.MAX_VALUE;
+    public int minZeroArray(int[] nums, int[][] queries) {
+        int l = 0, h = queries.length, res = Integer.MAX_VALUE;
 
         while (l <= h) {
             int mid = l + (h - l) / 2;
-            if (canMakeZero(nums, q, mid) == true) {
-                res = mid;
+            if (makeZero(nums, queries, mid) == true) {
+                res = Math.min(res, mid);
                 h = mid - 1;
             } else l = mid + 1;
         }
 
-        return res == Integer.MAX_VALUE ? -1: res;
+        return res == Integer.MAX_VALUE ? -1 : res;
+    }
+
+    public boolean makeZero(int[] nums, int[][] queries, int mid) {
+        int[] prefix = new int[nums.length];
+
+        for (int i = 0; i < mid; i++) {
+            int[] q = queries[i];
+            int l = q[0], h = q[1] + 1, val = q[2];
+            if (h < prefix.length) prefix[h] -= val;
+            prefix[l] += val;
+        }
+
+        if (nums[0] - prefix[0] > 0) return false;
+
+        for (int i = 1; i < nums.length; i++) {
+            prefix[i] = prefix[i] + prefix[i - 1];
+            if (nums[i] - prefix[i] > 0) return false;
+        }
+
+        return true;
     }
 }
