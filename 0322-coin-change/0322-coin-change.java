820ca3bdbd62ca5960d228c3d02b8ef01828b69c
mhk97 <149612134+mhk97@users.noreply.github.com>
2024-12-21 07:49:55 +0530

    Time: 29 ms (23.36%), Space: 46.2 MB (6.07%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -2,23 +2,24 @@ class Solution {
     Integer cache[][];
 
     public int coinChange(int[] coins, int amount) {
-        cache = new Integer[coins.length + 1][amount + 1];
-
+        Arrays.sort(coins);
+        cache = new Integer[coins.length + 1][10001];
         int res = solve(coins, amount, coins.length - 1);
-
-        return res == (int) 1e9 ? -1 : res;
+        return (res == (int) 1e8) ? -1 : res;
     }
 
     public int solve(int[] coins, int amount, int i) {
-        if (amount < 0) return (int) 1e9;
+        if (amount < 0) return (int) 1e8;
+
         if (amount == 0) return 0;
-        if (i == 0) {
-            return (amount % coins[i] == 0) ? amount / coins[i] : (int) 1e9;
-        }
 
         if (cache[i][amount] != null) return cache[i][amount];
 
-        int take = (coins[i] <= amount) ? 1 + solve(coins, amount - coins[i], i) : (int) 1e9;
+        if (i == 0) {
+            return (coins[i] <= amount && amount % coins[i] == 0) ? amount / coins[i] : (int) 1e8;
+        }
+
+        int take = (coins[i] <= amount) ? 1 + solve(coins, amount - coins[i], i) : (int) 1e8;
         int skip = solve(coins, amount, i - 1);
 
         return cache[i][amount] = Math.min(take, skip);
