0bb9f3337d0c1a7090b53d4645bdd19020dd3d73
mhk97 <149612134+mhk97@users.noreply.github.com>
2024-12-22 01:22:38 +0530

    Time: 1 ms (99.77%), Space: 47.5 MB (56.3%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,21 +1,32 @@
 class Solution {
-    int[][] dirs = new int[][] { { 0, 1 }, { 1, 0 } };
+    int[][] dirs = new int[][] { { 1, 0 }, { 0, 1 } };
+
     Integer cache[][];
+
     public int minPathSum(int[][] grid) {
         cache = new Integer[grid.length][grid[0].length];
         return solve(grid, 0, 0);
     }
 
-    public int solve(int[][] grid, int r, int c) {
-        if (r == grid.length - 1 && c == grid[0].length - 1) return grid[r][c];
-        if (r >= grid.length || r < 0 || c < 0 || c >= grid[0].length) return (int) 1e9;
-        if(cache[r][c]!= null) return cache[r][c];
-        int res = Integer.MAX_VALUE;
-        for (int i[] : dirs) {
-            int nr = i[0] + r, nc = i[1] + c;
-            res = Math.min(res, grid[r][c] + solve(grid, nr, nc));
+    public int solve(int[][] grid, int i, int j) {
+        int r = grid.length, c = grid[0].length;
+
+        if (i == r - 1 && j == c - 1) {
+            return grid[i][j];
         }
 
-        return cache[r][c] = res;
+        if (i >= r || j >= c) return (int) 1e8;
+
+        if (cache[i][j] != null) return cache[i][j];
+
+        int min = Integer.MAX_VALUE;
+
+        for (int d[] : dirs) {
+            int nr = d[0] + i, nc = d[1] + j;
+            int temp = grid[i][j] + solve(grid, nr, nc);
+            min = Math.min(min, temp);
+        }
+
+        return cache[i][j] = min;
     }
 }
