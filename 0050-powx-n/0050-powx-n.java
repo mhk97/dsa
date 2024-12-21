0ed1a36eed0dd7adfe9c7c147931f77458846779
mhk97 <149612134+mhk97@users.noreply.github.com>
2024-12-22 01:00:26 +0530

    Time: 0 ms (100%), Space: 43.4 MB (7.15%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -4,15 +4,16 @@ class Solution {
         if (x == 0) return 0;
 
         double res = solve(x, Math.abs(n));
-        return (n >= 0) ? res : (double) 1 / res;
+
+        return (n < 0) ? (double) 1 / res : res;
     }
 
     public double solve(double x, int n) {
         if (n == 0) return 1;
 
         double val = solve(x, n / 2);
-        double res = val*val;
+        val = (n % 2 == 0) ? val * val : val * val * x;
 
-        return (n % 2 == 0) ? res : res * x;
+        return val;
     }
 }
