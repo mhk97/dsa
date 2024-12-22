416b5ebabd9aaff8a823c36a04fb2ad92ffa22da
mhk97 <149612134+mhk97@users.noreply.github.com>
2024-12-22 14:59:56 +0530

    Time: 0 ms (100%), Space: 46 MB (30.73%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,45 +1,35 @@
 class Solution {
 
     public int[] searchRange(int[] nums, int target) {
-        int lb = findLB(nums, target), ub = findUB(nums, target);
+        int lb = findlowerbound(nums, target);
+        int ub = findupperbound(nums, target);
+
         return new int[] { lb, ub };
     }
 
-    public int findLB(int[] nums, int k) {
+    public int findlowerbound(int[] nums, int target) {
         int l = 0, h = nums.length - 1, res = Integer.MAX_VALUE;
 
         while (l <= h) {
             int mid = l + (h - l) / 2;
-            if (nums[mid] == k) {
+            if (nums[mid] == target) {
                 res = Math.min(res, mid);
                 h = mid - 1;
-            } else {
-                if (nums[mid] < k) {
-                    l = mid + 1;
-                } else {
-                    h = mid - 1;
-                }
-            }
+            } else if (nums[mid] < target) l = mid + 1; else h = mid - 1;
         }
 
         return res == Integer.MAX_VALUE ? -1 : res;
     }
 
-    public int findUB(int[] nums, int k) {
+    public int findupperbound(int[] nums, int target) {
         int l = 0, h = nums.length - 1, res = Integer.MIN_VALUE;
 
         while (l <= h) {
             int mid = l + (h - l) / 2;
-            if (nums[mid] == k) {
+            if (nums[mid] == target) {
                 res = Math.max(res, mid);
                 l = mid + 1;
-            } else {
-                if (nums[mid] < k) {
-                    l = mid + 1;
-                } else {
-                    h = mid - 1;
-                }
-            }
+            } else if (nums[mid] < target) l = mid + 1; else h = mid - 1;
         }
 
         return res == Integer.MIN_VALUE ? -1 : res;
