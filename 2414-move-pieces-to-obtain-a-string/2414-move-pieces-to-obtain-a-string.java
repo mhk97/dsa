205c0c4cd292cc453e5afd4cf12ea499f30c6836
mhk97 <149612134+mhk97@users.noreply.github.com>
2024-12-22 14:35:22 +0530

    Time: 15 ms (46.52%), Space: 45.5 MB (73.01%) - LeetHub

--- 2414-move-pieces-to-obtain-a-string/2414-move-pieces-to-obtain-a-string.java
+++ 2414-move-pieces-to-obtain-a-string/2414-move-pieces-to-obtain-a-string.java
@@ -3,9 +3,13 @@ class Solution {
     public boolean canChange(String start, String target) {
         int sid = 0, tid = 0, slen = start.length(), tlen = target.length();
 
-        while (sid < slen && tid < tlen) {
+        while (sid < slen || tid < tlen) {
             while (sid < slen && start.charAt(sid) == '_') sid++;
             while (tid < tlen && target.charAt(tid) == '_') tid++;
+
+            if (sid >= slen && tid >= tlen) return true;
+
+            if (sid >= tlen || tid >= tlen) return false;
 
             if (start.charAt(sid) != target.charAt(tid) || (start.charAt(sid) == 'L' && sid < tid) || (start.charAt(sid) == 'R' && sid > tid)) return false;
 
