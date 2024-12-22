e25cede5ce479e07846ac494100f83eec6fd86f5
mhk97 <149612134+mhk97@users.noreply.github.com>
2024-12-22 07:07:38 +0530

    Time: 2 ms (83.82%), Space: 44.6 MB (47.59%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,23 +1,23 @@
 class Solution {
     List<List<Integer>> res = new ArrayList();
 
-    public List<List<Integer>> combinationSum(int[] c, int t) {
-        solve(c, t, 0, new ArrayList());
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        solve(new ArrayList(), candidates, target, 0);
         return res;
     }
 
-    public void solve(int[] c, int t, int i, List<Integer> list) {
-        if (t == 0) {
+    public void solve(List<Integer> list, int[] c, int k, int idx) {
+        if (k == 0) {
             res.add(new ArrayList(list));
             return;
         }
 
-        if (t < 0 || i >= c.length) return;
+        if (idx >= c.length || k < 0) return;
 
-        list.add(c[i]);
-        solve(c, t - c[i], i, list); //take
+        list.add(c[idx]);
+        solve(list, c, k - c[idx], idx);
         list.remove(list.size() - 1);
 
-        solve(c, t, i + 1, list); //skip
+        solve(list, c, k, idx + 1);
     }
 }
