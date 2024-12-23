e5b6053f0635148933d3e38c72e2d7e11fb9fde1
mhk97 <149612134+mhk97@users.noreply.github.com>
2024-12-23 14:26:50 +0530

    Time: 2 ms (79.61%), Space: 46 MB (22.41%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,19 +1,14 @@
 class Solution {
-    Boolean cache[];
 
     public boolean canJump(int[] nums) {
-        cache = new Boolean[nums.length + 1];
-        return solve(nums, 0);
-    }
+        int maxSoFar = nums[0];
 
-    public boolean solve(int[] nums, int ind) {
-        if (ind >= nums.length - 1) return true;
-        if (cache[ind] != null) return cache[ind];
-
-        for (int i = 1; i <= nums[ind]; i++) {
-            if (solve(nums, ind + i) == true) return cache[ind] = true;
+        for (int i = 1; i < nums.length; i++) {
+            if (maxSoFar < i) return false;
+            maxSoFar = Math.max(maxSoFar, i + nums[i]);
+            if (maxSoFar >= nums.length - 1) return true;
         }
 
-        return cache[ind] = false;
+        return true;
     }
 }
