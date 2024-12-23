18ef6b799ba50e4898f0225943b5f762eb2b9e70
mhk97 <149612134+mhk97@users.noreply.github.com>
2024-12-23 17:17:09 +0530

    Time: 2 ms (23.6%), Space: 41.9 MB (14.52%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,13 +1,13 @@
 class Solution {
 
-    public int[] lps(String p) {
-        int n = p.length(), i = 1, len = 0;
-        int[] lps = new int[n];
+    public int[] lps(String key) {
+        int[] lps = new int[key.length()];
 
+        int len = 0, i = 1, n = key.length();
         lps[0] = 0;
 
         while (i < n) {
-            if (p.charAt(len) == p.charAt(i)) {
+            if (key.charAt(i) == key.charAt(len)) {
                 len++;
                 lps[i] = len;
                 i++;
@@ -26,7 +26,7 @@ class Solution {
 
     public int strStr(String s, String p) {
         int lps[] = lps(p);
-        System.out.println(Arrays.toString(lps(p)));
+        System.out.println(Arrays.toString(lps));
 
         int i = 0, j = 0, slen = s.length(), plen = p.length();
 
@@ -36,8 +36,9 @@ class Solution {
                 j++;
             }
 
-            if (j >= plen) return i - j; else if (i < slen && (s.charAt(i) != p.charAt(j))) {
-                if (j > 0) j = lps[j - 1]; else i++;
+            if (j >= plen) return i - j;
+            else if (i < slen && (s.charAt(i) != p.charAt(j))) {
+            if (j > 0) j = lps[j - 1]; else i++;
             }
         }
 
