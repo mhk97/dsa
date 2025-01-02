e0efc9b36ccd7b0b994f4cedaa1dd14ab8f48db1
mhk97 <149612134+mhk97@users.noreply.github.com>
2025-01-02 20:57:32 +0530

    Time: 1 ms (92.8%), Space: 45.4 MB (95.96%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,22 +1,12 @@
 class Solution {
-    Integer cache[][];
 
-    public int maxProfit(int[] prices) {
-        cache = new Integer[prices.length + 1][2];
-        return solve(prices, 0, 1);
-    }
+    public int maxProfit(int[] p) {
+        int res = 0;
 
-    public int solve(int[] nums, int ind, int buy) {
-        if (ind >= nums.length) return 0;
-        if (cache[ind][buy] != null) return cache[ind][buy];
-        int profit = 0;
-
-        if (buy == 1) {
-            profit = Math.max(-nums[ind] + solve(nums, ind + 1, 0), 0 + solve(nums, ind + 1, 1));
-        } else {
-            profit = Math.max(nums[ind] + solve(nums, ind + 1, 1), 0 + solve(nums, ind + 1, 0));
+        for (int i = 1; i < p.length; i++) {
+            if (p[i] > p[i - 1]) res += (p[i] - p[i - 1]);
         }
 
-        return cache[ind][buy] = profit;
+        return res;
     }
 }
