0a805612b72d18c8d1aa9db9122bf435cd1b1ad7
mhk97 <149612134+mhk97@users.noreply.github.com>
2025-01-02 20:40:10 +0530

    Time: 17 ms (7.95%), Space: 45.3 MB (6.66%) - LeetHub

--- 1085-the-earliest-moment-when-everyone-become-friends/1085-the-earliest-moment-when-everyone-become-friends.java
+++ 1085-the-earliest-moment-when-everyone-become-friends/1085-the-earliest-moment-when-everyone-become-friends.java
@@ -1,37 +1,37 @@
 class Solution {
 
     public int earliestAcq(int[][] logs, int n) {
-        Map<Integer, List<int[]>> map = new HashMap();
+        //prims
 
-        for (int i[] : logs) {
-            int u = i[1], v = i[2], val = i[0];
-            map.computeIfAbsent(u, k -> new ArrayList()).add(new int[] { val, v });
-            map.computeIfAbsent(v, k -> new ArrayList()).add(new int[] { val, u });
+        Map<Integer, List<Pair<Integer, Integer>>> map = new HashMap();
+
+        for (int[] i : logs) {
+            int u = i[1], v = i[2], cost = i[0];
+            map.computeIfAbsent(u, k -> new ArrayList()).add(new Pair(cost, v));
+            map.computeIfAbsent(v, k -> new ArrayList()).add(new Pair(cost, u));
         }
 
-        PriorityQueue<int[]> pq = new PriorityQueue<int[]>((a, b) -> Integer.compare(a[0], b[0]));
+        PriorityQueue<Pair<Integer, Integer>> pq = new PriorityQueue<>((a, b) -> Integer.compare(a.getKey(), b.getKey()));
 
-        pq.add(new int[] { -1, 0 });
+        pq.offer(new Pair(-1, 0));
 
         int res = Integer.MIN_VALUE;
-        boolean visited[] = new boolean[n];
+        boolean[] visited = new boolean[n];
 
         while (!pq.isEmpty()) {
-            int[] temp = pq.poll();
-            int node = temp[1], cost = temp[0];
-            if (visited[node] == false) {
+            Pair<Integer, Integer> p = pq.poll();
+            int cost = p.getKey(), u = p.getValue();
+            if (visited[u] == false) {
+                visited[u] = true;
                 res = Math.max(res, cost);
-                visited[node] = true;
-                for (int v[] : map.getOrDefault(node, new ArrayList<int[]>())) {
-                    if (visited[v[1]] == false) {
-                        pq.add(v);
-                    }
+                for (Pair<Integer, Integer> temp : map.getOrDefault(u, new ArrayList<Pair<Integer, Integer>>())) {
+                    int vcost = temp.getKey(), v = temp.getValue();
+                    if (visited[v] == false) pq.offer(temp);
                 }
             }
         }
 
-        for(boolean flag: visited) if(flag == false) return -1;
-
+        for (boolean f : visited) if (f == false) return -1;
         return res;
     }
 }
