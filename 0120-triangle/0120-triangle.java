897fe30f402c25eff641eafe2ad041023773643c
mhk97 <149612134+mhk97@users.noreply.github.com>
2025-01-02 12:46:44 +0530

    Time: 6 ms (29.23%), Space: 45.8 MB (5.15%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,17 +1,20 @@
 class Solution {
-    Integer[][] cache;
 
     public int minimumTotal(List<List<Integer>> t) {
-        cache = new Integer[201][201];
-        return solve(t, 0, 0);
-    }
+        int row = t.size();
 
-    public int solve(List<List<Integer>> t, int i, int j) {
-        if (cache[i][j] != null) return cache[i][j];
-        int k = t.get(i).get(j);
+        List<Integer> last = t.get(row - 1);
 
-        if (i < t.size() - 1) k += Math.min(solve(t, i + 1, j), solve(t, i + 1, j + 1));
+        for (int i = row - 2; i >= 0; i--) {
+            List<Integer> curr = t.get(i);
+            List<Integer> temp = new ArrayList();
+            for (int j = 0; j < curr.size(); j++) {
+                int min = Math.min(last.get(j), last.get(j + 1));
+                temp.add(min + curr.get(j));
+            }
+            last = temp;
+        }
 
-        return cache[i][j] = k;
+        return last.get(0);
     }
 }
