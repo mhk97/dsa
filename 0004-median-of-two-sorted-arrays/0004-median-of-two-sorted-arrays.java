4d6740431813081cdb3245fa43123f88747439d9
mhk97 <149612134+mhk97@users.noreply.github.com>
2025-01-02 22:57:32 +0530

    Time: 1 ms (100%), Space: 46.5 MB (8.44%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,27 +1,29 @@
 class Solution {
 
-    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int n1 = nums1.length, n2 = nums2.length;
-
+    public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int n1 = nums1.length, n2 = nums2.length, total = n1 + n2;
         if (n1 > n2) return findMedianSortedArrays(nums2, nums1);
 
-        int l = 0, h = n1, partition = (n1 + n2 + 1) / 2;
+        int partition = (total + 1) / 2;
+
+        int l = 0, h = n1;
 
         while (l <= h) {
-            int mid1 = l + (h - l) / 2;
-            int mid2 = partition - mid1;
-            int l1 = (mid1 - 1 >= 0) ? nums1[mid1 - 1] : Integer.MIN_VALUE;
-            int r1 = (mid1 < n1) ? nums1[mid1] : Integer.MAX_VALUE;
-            int l2 = (mid2 - 1 >= 0) ? nums2[mid2 - 1] : Integer.MIN_VALUE;
-            int r2 = (mid2 < n2) ? nums2[mid2] : Integer.MAX_VALUE;
+            int mid = l + (h - l) / 2;
+            int h1 = (mid - 1 >= 0) ? nums1[mid - 1] : Integer.MIN_VALUE;
+            int h2 = (partition - mid - 1 >= 0 && partition - mid - 1 < n2) ? nums2[partition - mid - 1] : Integer.MIN_VALUE;
+            int l1 = (mid >= 0 && mid < n1) ? nums1[mid] : Integer.MAX_VALUE;
+            int l2 = (partition - mid < n2) ? nums2[partition - mid] : Integer.MAX_VALUE;
 
-            if (l1 > r2) h = mid1 - 1; else if (l2 > r1) l = mid1 + 1; else {
-                if ((n1 + n2) % 2 == 1) {
-                    return Math.max(l1, l2);
-                } else return (double) (Math.max(l1, l2) + Math.min(r1, r2)) / 2;
-            }
+            if (h1 <= l2 && h2 <= l1) {
+                if (total % 2 == 1) return Math.max(h1, h2); else {
+                    int min = Math.max(h1, h2);
+                    int max = Math.min(l1, l2);
+                    return (double) (min + max) / 2;
+                }
+            } else if (h2 > l1) l = mid + 1; else h = mid - 1;
         }
 
-        return -1;
+        return 0d;
     }
 }
