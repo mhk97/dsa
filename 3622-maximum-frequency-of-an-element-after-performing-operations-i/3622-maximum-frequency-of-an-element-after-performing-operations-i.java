1c22f5751ac5e719b76f3b15fedcb9c39ff48dfd
mhk97 <149612134+mhk97@users.noreply.github.com>
2025-01-03 08:51:51 +0530

    Time: 874 ms (5.2%), Space: 74 MB (12.79%) - LeetHub

--- 3622-maximum-frequency-of-an-element-after-performing-operations-i/3622-maximum-frequency-of-an-element-after-performing-operations-i.java
+++ 3622-maximum-frequency-of-an-element-after-performing-operations-i/3622-maximum-frequency-of-an-element-after-performing-operations-i.java
@@ -2,28 +2,25 @@ class Solution {
 
     public int maxFrequency(int[] nums, int k, int no) {
         Map<Integer, Integer> map = new HashMap();
-
-        for (int i : nums) map.put(i, map.getOrDefault(i, 0) + 1);
-
-        Map<Integer, Integer> sweep = new TreeMap();
+        Map<Integer, Integer> range = new TreeMap();
 
         for (int i : nums) {
+            map.put(i, map.getOrDefault(i, 0) + 1);
             int l = i - k, h = i + k + 1;
-
-            sweep.put(l, sweep.getOrDefault(l, 0) + 1);
-            sweep.put(i, sweep.getOrDefault(i, 0));
-            sweep.put(h, sweep.getOrDefault(h, 0) - 1);
+            range.put(l, range.getOrDefault(l, 0) + 1);
+            range.put(i, range.getOrDefault(i, 0));
+            range.put(h, range.getOrDefault(h, 0) - 1);
         }
 
-        int res = Integer.MIN_VALUE, prev = 0;
+        int res = 0, prev = 0, prefix = 0;
 
-        for (Map.Entry<Integer, Integer> entry : sweep.entrySet()) {
-            int key = entry.getKey(), value = entry.getValue(), prefix = prev + value;
+        for (Map.Entry<Integer, Integer> entry : range.entrySet()) {
+            int num = entry.getKey(), freq = entry.getValue();
+            prefix = prev + freq;
+            int ogfre = map.getOrDefault(num, 0);
 
-            int existing = map.getOrDefault(key, 0);
-            int ops = Math.min(prefix - existing, no);
-            res = Math.max(res, ops + existing);
-
+            int ops = Math.min(no, (prefix - ogfre));
+            res = Math.max(res, ops + ogfre);
             prev = prefix;
         }
 
