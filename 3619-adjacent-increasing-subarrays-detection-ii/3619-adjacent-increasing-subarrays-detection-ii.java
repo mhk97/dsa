02a859669c8b70d6fd878a7a98f0370a6af4c2b0
mhk97 <149612134+mhk97@users.noreply.github.com>
2025-01-03 09:35:04 +0530

    Time: 21 ms (39.55%), Space: 90.4 MB (39.7%) - LeetHub

--- 3619-adjacent-increasing-subarrays-detection-ii/3619-adjacent-increasing-subarrays-detection-ii.java
+++ 3619-adjacent-increasing-subarrays-detection-ii/3619-adjacent-increasing-subarrays-detection-ii.java
@@ -1,35 +1,32 @@
 class Solution {
 
     public int maxIncreasingSubarrays(List<Integer> nums) {
-        int n = nums.size(), res = 1;
-        int prefix[] = new int[n];
+        int n = nums.size();
 
+        int prefix[] = new int[n];
         prefix[n - 1] = 1;
+
         for (int i = n - 2; i >= 0; i--) {
-            if (nums.get(i) >= nums.get(i + 1)) {
-                prefix[i] = 1;
-            } else prefix[i] = prefix[i + 1] + 1;
+            if (nums.get(i) < nums.get(i + 1)) prefix[i] = prefix[i + 1] + 1; else prefix[i] = 1;
         }
 
-        int l = 1, h = n / 2;
+        int l = 0, h = n / 2, res = Integer.MIN_VALUE;
+
         while (l <= h) {
             int mid = l + (h - l) / 2;
-
-            if (strictlyIncreasing(prefix, mid) == true) {
+            if (check(prefix, mid) == true) {
                 res = Math.max(res, mid);
                 l = mid + 1;
-            }
-            else h = mid - 1;
+            } else h = mid - 1;
         }
 
         return res;
     }
 
-    public boolean strictlyIncreasing(int[] prefix, int k) {
-        for (int i = 0; i <= prefix.length - 2 * k; i++) {
-            if (prefix[i] >= k && prefix[i + k] >= k) return true;
+    public boolean check(int[] prefix, int mid) {
+        for (int i = 0; i < prefix.length - mid; i++) {
+            if (prefix[i] >= mid && prefix[i + mid] >= mid) return true;
         }
-
         return false;
     }
 }
