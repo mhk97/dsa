0b06190af794b01c313388d9f3fd1da6a4006a9b
mhk97 <149612134+mhk97@users.noreply.github.com>
2025-01-03 07:51:48 +0530

    Time: 201 ms (67.97%), Space: 44.9 MB (73.36%) - LeetHub

--- 0976-minimum-area-rectangle/0976-minimum-area-rectangle.java
+++ 0976-minimum-area-rectangle/0976-minimum-area-rectangle.java
@@ -1,24 +1,21 @@
 class Solution {
 
-    public int minAreaRect(int[][] points) {
-        Map<Integer, Set<Integer>> map = new HashMap();
+    public int minAreaRect(int[][] p) {
+        HashMap<Integer, Set<Integer>> map = new HashMap();
 
-        for (int i[] : points) {
-            int x = i[0], y = i[1];
-
-            map.computeIfAbsent(x, k -> new HashSet()).add(y);
+        for (int i[] : p) {
+            map.computeIfAbsent(i[0], k -> new HashSet()).add(i[1]);
         }
 
         int res = Integer.MAX_VALUE;
-
-        for (int i = 0; i < points.length; i++) {
-            int x1 = points[i][0], y1 = points[i][1];
-            for (int j = i + 1; j < points.length; j++) {
-                int x2 = points[j][0], y2 = points[j][1];
+        for (int i = 0; i < p.length; i++) {
+            for (int j = i + 1; j < p.length; j++) {
+                int x1 = p[i][0], y1 = p[i][1];
+                int x2 = p[j][0], y2 = p[j][1];
                 if (x1 != x2 && y1 != y2) {
                     if (map.get(x1).contains(y2) && map.get(x2).contains(y1)) {
                         int area = Math.abs(x2 - x1) * Math.abs(y2 - y1);
-                        res = Math.min(area, res);
+                        res = Math.min(res, area);
                     }
                 }
             }
