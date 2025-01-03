8e61855a6e557d2a82451c6014033abab18e04ef
mhk97 <149612134+mhk97@users.noreply.github.com>
2025-01-03 07:55:19 +0530

    Time: 9 ms (31.53%), Space: 46.1 MB (40.71%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.java
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.java
@@ -1,17 +1,12 @@
 class Solution {
 
     public int minMeetingRooms(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
         PriorityQueue<Integer> pq = new PriorityQueue();
 
-        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
-
-        pq.offer(intervals[0][1]);
-
-        for (int i = 1; i < intervals.length; i++) {
-            int start = intervals[i][0], end = intervals[i][1];
-
+        for (int i[] : intervals) {
+            int start = i[0], end = i[1];
             if (!pq.isEmpty() && pq.peek() <= start) pq.poll();
-
             pq.offer(end);
         }
 
