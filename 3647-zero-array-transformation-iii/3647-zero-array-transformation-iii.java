cee903f45ae7881b1cea39282c9b3eb3b12cf65d
mhk97 <149612134+mhk97@users.noreply.github.com>
2025-01-04 15:57:45 +0530

    Time: 93 ms (24.15%), Space: 114.2 MB (5.38%) - LeetHub

--- 3647-zero-array-transformation-iii/3647-zero-array-transformation-iii.java
+++ 3647-zero-array-transformation-iii/3647-zero-array-transformation-iii.java
@@ -1,30 +1,32 @@
 class Solution {
 
     public int maxRemoval(int[] nums, int[][] q) {
+        PriorityQueue<Integer> candidates = new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Integer> chosen = new PriorityQueue<>();
+
         Arrays.sort(q, (a, b) -> Integer.compare(a[0], b[0]));
+        int res = 0, j = 0;
 
-        PriorityQueue<Integer> candidate = new PriorityQueue(Collections.reverseOrder());
-        PriorityQueue<Integer> chosen = new PriorityQueue();
-
-        int res = 0, n = nums.length, j = 0;
-
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < nums.length; i++) {
+            //add to candidates for this index
             while (j < q.length && q[j][0] == i) {
-                candidate.offer(q[j][1]);
+                candidates.offer(q[j][1]);
                 j++;
             }
 
             nums[i] -= chosen.size();
 
-            while (nums[i] > 0 && !candidate.isEmpty() && candidate.peek() >= i) {
+            while (nums[i] > 0 && !candidates.isEmpty() && candidates.peek() >= i) {
+                chosen.add(candidates.poll());
                 res++;
-                chosen.offer(candidate.poll());
                 nums[i]--;
             }
 
             if (nums[i] > 0) return -1;
 
-            while (!chosen.isEmpty() && chosen.peek() == i) chosen.poll();
+            while (!chosen.isEmpty() && chosen.peek() == i) {
+                chosen.poll();
+            }
         }
 
         return q.length - res;
