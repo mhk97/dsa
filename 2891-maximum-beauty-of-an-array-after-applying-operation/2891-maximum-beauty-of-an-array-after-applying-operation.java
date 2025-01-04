955d3996e282757808bc88877020b6445c3a9a06
mhk97 <149612134+mhk97@users.noreply.github.com>
2025-01-04 16:05:57 +0530

    Time: 208 ms (5.04%), Space: 61.8 MB (12.45%) - LeetHub

--- 2891-maximum-beauty-of-an-array-after-applying-operation/2891-maximum-beauty-of-an-array-after-applying-operation.java
+++ 2891-maximum-beauty-of-an-array-after-applying-operation/2891-maximum-beauty-of-an-array-after-applying-operation.java
@@ -1,14 +1,23 @@
 class Solution {
 
     public int maximumBeauty(int[] nums, int k) {
-        Arrays.sort(nums);
-        int i = 0, j = 0, n = nums.length, res = Integer.MIN_VALUE;
+        int n = nums.length;
+        int[][] ranges = new int[n][2];
 
-        while (j < n) {
-            while (j < n && nums[j] <= (nums[i] + 2 * k)) j++;
+        for (int i = 0; i < n; i++) {
+            int curr = nums[i];
+            ranges[i] = new int[] { curr - k, curr + k };
+        }
 
-            res = Math.max(res, j - i);
-            i++;
+        Arrays.sort(ranges, (a, b) -> Integer.compare(a[0], b[0]));
+
+        PriorityQueue<Integer> pq = new PriorityQueue();
+        int res = Integer.MIN_VALUE;
+
+        for (int i[] : ranges) {
+            while (!pq.isEmpty() && pq.peek() < i[0]) pq.poll();
+            pq.add(i[1]);
+            res = Math.max(res, pq.size());
         }
 
         return res;
