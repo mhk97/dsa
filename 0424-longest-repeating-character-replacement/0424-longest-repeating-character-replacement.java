80837e5e30ff2c0bd688d6f44485bf0cd172b6be
mhk97 <149612134+mhk97@users.noreply.github.com>
2025-01-05 17:34:22 +0530

    Time: 87 ms (5.04%), Space: 45.8 MB (5.44%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,25 +1,20 @@
 class Solution {
 
     public int characterReplacement(String s, int k) {
-        int i = 0, j = 0, res = 0, max = 0, n = s.length();
         Map<Character, Integer> map = new HashMap();
+
+        int i = 0, j = 0, n = s.length(), res = 0;
 
         while (j < n) {
             char curr = s.charAt(j);
+            map.put(curr, map.getOrDefault(curr, 0) + 1);
 
-            int count = map.getOrDefault(curr, 0) + 1;
-            map.put(curr, count);
-
-            max = Math.max(max, count);
-
-            if (j - i + 1 - max <= k) {
+            if (j - i + 1 - findMax(map) <= k) {
                 res = Math.max(res, j - i + 1);
             } else {
-                while (j - i + 1 - max > k) {
+                while ((j - i + 1) - findMax(map) > k) {
                     char icurr = s.charAt(i++);
-                    int icount = map.getOrDefault(icurr, 0) - 1;
-                    if (icount <= 0) map.remove(icurr); else map.put(icurr, icount);
-                    max = findMax(map);
+                    map.put(icurr, map.getOrDefault(icurr, 0) - 1);
                 }
             }
 
@@ -31,7 +26,10 @@ class Solution {
 
     public int findMax(Map<Character, Integer> map) {
         int max = Integer.MIN_VALUE;
-        for (int i : map.values()) max = Math.max(i, max);
+        for (int i : map.values()) {
+            max = Math.max(max, i);
+        }
+
         return max;
     }
 }
