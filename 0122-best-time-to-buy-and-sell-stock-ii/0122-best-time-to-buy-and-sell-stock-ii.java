98006bcf515fe4d22cee3de3ad8c4f9ef2f67fb6
mhk97 <149612134+mhk97@users.noreply.github.com>
2025-01-05 15:57:23 +0530

    Time: 1 ms (92.67%), Space: 46 MB (28.78%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,10 +1,11 @@
 class Solution {
 
     public int maxProfit(int[] p) {
+
         int res = 0;
 
         for (int i = 1; i < p.length; i++) {
-            if (p[i] > p[i - 1]) res += (p[i] - p[i - 1]);
+            if(p[i] > p[i-1]) res+= (p[i] - p[i-1]);
         }
 
         return res;
